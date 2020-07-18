43452cb9c0d13b43af931199da7f8d0b2e7c2fca
Your Name <you@example.com>
2020-07-18 23:30:39 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                const ImagesAndIconWidget(),
+                Divider(),
+                const BoxDecoratorWidget(),
+                Divider(),
+                const InputDecoratorsWidget(),
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+class ImagesAndIconWidget extends StatelessWidget {
+ const ImagesAndIconWidget({
+   Key  key,
+ }): super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Image(
+          image: AssetImage("assets/images/logo.jpg"),
+          fit: BoxFit.cover,
+          width: MediaQuery.of(context).size.width/3,
+        ),
+        Image.network(
+          'https://flutter.io/images/catalog-widget-placeholder.png',
+          width: MediaQuery.of(context).size.width / 3,
+        ),
+        Icon(
+          Icons.brush,
+          color: Colors.lightBlue,
+          size: 48.0,
+        ),
+      ],
+    );
+  }
+}
+ class BoxDecoratorWidget extends StatelessWidget {
+   const BoxDecoratorWidget({
+     Key key,
+   }): super(key:key);
+   
+   @override
+   Widget build(BuildContext context){
+     return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+          color: Colors.orange,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 10.0,
+              offset: Offset(10.0,15.0),
+            )
+          ],
+        ),
+      ),
+     );
+   }
+ }
+ class InputDecoratorsWidget  extends StatelessWidget {
+   const InputDecoratorsWidget({
+     Key key
+   }): super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+     return Column(
+       children: <Widget>[
+         TextField(
+           keyboardType: TextInputType.text,
+           style: TextStyle(
+             color: Colors.grey.shade800,
+             fontSize: 16.0,
+           ),
+           decoration: InputDecoration(
+             labelText: "Notes",
+             labelStyle: TextStyle(color: Colors.purple),
+             border: OutlineInputBorder(),
+           ),
+         ),
+         Divider(
+           color: Colors.lightGreen,
+           height: 50.0,
+         ),
+         TextFormField(
+           decoration: InputDecoration(labelText: 'Enter your notes'),
+         ),
+       ],
+     );
+   }
+ }
